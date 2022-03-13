a0d08840ab3aabb8ecf5114687810723e20fc5fa
Sandeep Vittala <applesandy33@gmail.com>
2022-03-13 13:34:57 -0400

    thought model

---
+++ models/Thought.js
@@ -0,0 +1,70 @@
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+
+const ReactionSchema = new Schema(
+    {
+      // set custom id to avoid confusion with parent thought _id
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+      },
+      reactionBody: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 1,
+        maxlength: 280
+      },
+      username: {
+        type: String,
+        required: true
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format('llll')
+      }
+    },
+    {
+      toJSON: {
+        getters: true
+      }
+    }
+  );
+  
+  const ThoughtSchema = new Schema(
+    {
+      thoughtText: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxlength: 280
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format('llll')
+      },
+      username: {
+        type: String,
+        required: true,
+        ref: 'User'
+      },
+      reactions: [ReplySchema]
+    },
+    {
+      toJSON: {
+        virtuals: true,
+        getters: true
+      },
+      id: false
+    }
+  );
+  
+  ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+  });
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
